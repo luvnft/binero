c7f6e3ea5f6393134aa8b27973182598c318a161
Pavel Lozinsky <pavel@lozinsky.dev>
2024-01-28 11:31:53 +0100

    feat: add timeout for board generation

--- app/routes/game.new.$size/route.tsx
+++ app/routes/game.new.$size/route.tsx
@@ -8,7 +8,10 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
   try {
     const url = new URL(request.url);
     const board = await generateBoard(expectNotToBeNaN(Number(params.size)), {
-      signal: request.signal,
+      /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
+      // @ts-expect-error https://github.com/DefinitelyTyped/DefinitelyTyped/issues/60868
+      signal: AbortSignal.any([request.signal, AbortSignal.timeout(5000)]),
+      /* eslint-enable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call */
     });
 
     url.pathname = `/game/${board.toString()}`;
