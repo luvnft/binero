3ac192f8c021d55fc74346c722c9810193f90943
Pavel Lozinsky <pavel@lozinsky.dev>
2024-02-07 09:23:37 +0100

    chore: update `MatrixSelection` test

--- app/lib/matrix/matrix-selection.test.ts
+++ app/lib/matrix/matrix-selection.test.ts
@@ -18,7 +18,7 @@ test.each([
     ],
     [1, 1, 4, 5],
   ],
-])('returns matrix selection', (matrix, cells) => {
+])('returns matrix selection from cells', (matrix, cells) => {
   expect(MatrixSelection.from(matrix, cells).execute(matrix)).toMatchSnapshot();
 });
 
@@ -52,6 +52,25 @@ test.each([
       { x: 2, y: 2 },
     ]),
   ],
-])('returns matrix cells', (matrix, selection) => {
+])('returns matrix selection cells', (matrix, selection) => {
   expect(selection.execute(matrix)).toMatchSnapshot();
+});
+
+test.each([
+  new MatrixSelection([
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 1 },
+    { x: 1, y: 1 },
+    { x: 0, y: 2 },
+    { x: 2, y: 2 },
+  ]),
+  new MatrixSelection([
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+    { x: 0, y: 1 },
+    { x: 1, y: 1 },
+  ]),
+])('returns matrix selection value of', (selection) => {
+  expect(selection.valueOf()).toMatchSnapshot();
 });
